6e24ad123c360bcdee2d4f73a5305ff7211dda0c
Deeps <60666214+deeps-sf@users.noreply.github.com>
2025-03-17 15:19:14 -0500

    Update outputs.tf

--- examples/terraform-aws-ec2-windows-example/outputs.tf
+++ examples/terraform-aws-ec2-windows-example/outputs.tf
@@ -1,4 +1,4 @@
 output "windows_instance_public_ip" {
-  description = "The IPv4 address of the Windows instance. Enter this value into your RDP client when connecting to your instance."
+  description = "The IPv4 address of the Windows instance. Enter this value into your RDP client when connecting to your instance.--Test1"
   value       = aws_instance.instance.public_ip
 }
